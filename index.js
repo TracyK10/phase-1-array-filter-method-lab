eed9c3e1b8b0c8b1c50f433387e8cd9c86b2f9a1
Tracy Karanja <chirutracy@gmail.com>
2024-03-24 19:33:51 +0300

    Initial commit

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution here
+
+//returns an array if the string argument matches
+function findMatching(arr, name){
+    function findName(n) {  
+        return n.toUpperCase() === name.toUpperCase(); }
+
+    return arr.filter(findName);
+}
+
+// console.log(findMatching(drivers,'bobby'))
+
+//returns an array if the element begins with a specific letter
+function fuzzyMatch(arr, begin){
+    return arr.filter(word => word.startsWith(begin))
+}
+// console.log(fuzzyMatch(drivers,"Sa")) 
+
+//finding object from array where the string argument matches the name key in the array
+function matchName(arr, driver){
+    return arr.filter(dname => dname.name === driver);
+}
+
+// console.log(matchName(drivers, "Bobby"));
